ed5b5829cfbdf1f237b62a1f606f67ffc1ec4595
Jonathan Lartey <lightlartey2000@gmail.com>
2025-06-01 20:44:37 +0000

    Added match case calculator app

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,32 @@
+"""
+A pyhton script that will prompt the user to enter two numbers and select an operation (addition
+,subtraction. multiplication or division).
+The script will perform the selected operation usinf a Match case statement and display the results.
+
+Input will be an int type.
+variables.
+First and second number.
+Operand
+"""
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+operand =input("Choose the operation (+, -, *, /):.")
+
+match operand:
+    case '+':
+        print(f"The result is {num1 + num2}.")
+    case '-':
+        print(f"The result is {num1 - num2}.")
+    case '*':
+        print(f"The result is {num1 * num2}.")
+    case '/':
+        try:
+            result = num1 / num2
+            print(f"The result is {result}.")
+        except ZeroDivisionError:
+            print("Cannot divide by zero.")
+
+    case _:
+        print("Incorrect operation entered")
