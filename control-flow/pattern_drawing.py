915e72022047f4f3da02fc9a40cad4eb6de9f9fd
Jonathan Lartey <lightlartey2000@gmail.com>
2025-06-02 00:15:08 +0000

    Added pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+while True:
+    size = input("Enter the size of the pattern: ")
+    if size.lower() == 'exit':
+        break
+    if size.isdigit():
+        for _ in range(int(size)):
+            print("*" * int(size))
+    else:
+        print("Invalid input. Enter a number")
