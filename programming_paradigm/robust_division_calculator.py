c969a71417b8ec28dcdf0b60246b3e2d82ed8d55
Jonathan Lartey <lightlartey2000@gmail.com>
2025-06-22 13:55:21 +0000

    Created a robust division calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,15 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        denom = float(denominator)
+
+    except ValueError:
+        return "Error: Please enter numeric values only."
+
+    try:
+        divide = num / denom
+
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+
+    return f"The result of the division is {divide}"
