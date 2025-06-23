4015014896280f08390a05d1035ae2ce5521ddc2
Jonathan Lartey <lightlartey2000@gmail.com>
2025-06-23 22:03:08 +0000

    Created library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,21 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self_is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book)
+        self.book.append(book).
